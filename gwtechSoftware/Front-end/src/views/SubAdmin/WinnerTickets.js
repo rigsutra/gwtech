455230f11b262845d5b906ace4bf21077f384ec0
Ashish Singh <110900078+rigsutra@users.noreply.github.com>
2024-10-12 15:57:12 +0530

    Update WinnerTickets.js

--- gwtechSoftware/Front-end/src/views/SubAdmin/WinnerTickets.js
+++ gwtechSoftware/Front-end/src/views/SubAdmin/WinnerTickets.js
@@ -94,6 +94,7 @@ const WinnerTickets = () => {
       const response = await api().get(
         `/subadmin/getwintickets?seller=${selectedSellerId}&fromDate=${fromDate}&toDate=${toDate}&lotteryCategoryName=${lotteryCategoryName.trim()}`
       );
+      console.log(response);
       setWinnerTickets(response.data.data);
     } catch (error) {
       console.error(error);
